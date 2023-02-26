f1373f7c1b0c36f816b744134ef71286691bcfbf
Joe Pasqua <joe@noroomattheinn.org>
2023-02-26 08:30:04 -0800

    FS_NO_GLOBALS workaround
    Because some libraries define FS_NO_GLOBALS, we need this ugly workaround. An example of such a library is TFT_eSPI. Refer to esp8266/Arduino/#6286

--- src/WebUI.h
+++ src/WebUI.h
@@ -9,6 +9,14 @@
 #define WebUI_h
 
 //--------------- Begin:  Includes ---------------------------------------------
+//                                  WORKAROUNDS
+// Because some libraries define FS_NO_GLOBALS, we need this ugly workaround
+// An example of such a library is TFT_eSPI. Refer to this github issue.
+// https://github.com/esp8266/Arduino/issues/6286
+// Such libraries should be fixed, but we have no control over that.
+#include <FS.h>
+using File = fs::File;
+using FS = fs::FS;
 //                                  Core Libraries
 #if defined(ESP8266)
   #include <ESP8266WebServer.h>
